899d26246ea00d20733fbed8b9e188311f9cbf19
Chuong Ngo <cngo.github@gmail.com>
2014-02-14 16:43:58 -0500

    Modified server so that it will hold onto requests and send them later.

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 var http = require('http');
+var requests = [];
+
+function processRequest() {
+	while(requests.length) {
+		response = requests.shift();
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.end('Hello World!');
+	}
+}
 
 function start() {
 	function onRequest(request, response) {
-		response.writeHead(200, {'Content-Type': 'text/plain'});
-		response.write('Hello World');
-		response.end();
+		// Store the responses for later use.
+		requests.push(response);
 	}
 
 	http.createServer(onRequest).listen(8888);
+	setInterval(processRequest, 2000);
 	console.log('Server has started.');
 }
 
